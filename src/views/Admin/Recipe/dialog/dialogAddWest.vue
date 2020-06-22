21e98c5a903d57884d7ee72ffb22ef0598607442
hsl <Huang_senliang@163.com>
2020-06-22 12:21:45 +0800

    管理页面布局，中药处方

---
+++ src/views/Admin/Recipe/dialog/dialogAddWest.vue
@@ -0,0 +1,75 @@
+<template>
+  <!-- 添加西方处方 -->
+  <div class="dialog-add-west dialog">
+    <div class="cover-wrapper"></div>
+    <div class="content-style">
+      <!-- 头部 -->
+      <div class="add-west-header flex align-items justify-between">
+        <h4>新增处方模板</h4>
+        <i class="iconfont icon-del"></i>
+      </div>
+      <!-- 内容 -->
+      <div class="add-west-body">
+        <div class="form-wrapper">
+          <el-form label-position="left" label-width="80px" :model="form">
+            <el-form-item label="模板名称">
+              <el-input style="width:400px" v-model="form.name"></el-input>
+            </el-form-item>
+            <el-form-item label="模板归属">
+              <el-radio v-model="form.radioValue" label="1" style="marginRight:15px">诊所公用</el-radio>
+              <el-radio v-model="form.radioValue" label="2">个人专用</el-radio>
+            </el-form-item>
+            <el-form-item label="主治病症">
+              <el-input style="width:400px" v-model="form.symptoms"></el-input>
+            </el-form-item>
+          </el-form>
+        </div>
+        <div class="line"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: "", // 模板名字
+        radioValue: "1", // 模板归属
+        symptoms: "" // 主治病症
+      } // 表单数据
+    };
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.dialog-add-west {
+  .content-style {
+    width: 824px;
+    background: #fff;
+    // 头部
+    .add-west-header {
+      padding-left: 24px;
+      height: 41px;
+      border-bottom: 1px solid #e6eaee;
+      background-color: #eff1f5;
+      border-top-right-radius: 3px;
+      border-top-left-radius: 3px;
+      h4 {
+        font-size: 16px;
+        font-weight: 700;
+        color: #000;
+      }
+    }
+    // 内容
+    .add-west-body {
+			padding: 24px;
+			.form-wrapper{
+				margin-bottom: 24px;
+			}
+    }
+  }
+}
+</style>
--- src/views/Admin/Recipe/recipeWest.vue
+++ src/views/Admin/Recipe/recipeWest.vue
@@ -4,61 +4,60 @@
     <Admin-Header title="西药处方"></Admin-Header>
     <div class="west-body">
       <!-- 顶部按钮 -->
-      <div class="btn-wrapper flex align-items">
-        <button class="btn-success" style="marginRight:15px">新增</button>
+      <div class="header-wrapper flex align-items">
+        <button class="btn-success" style="marginRight:10px">新增</button>
+        <!-- 添加处方 -->
+        <Dialog-Add-West></Dialog-Add-West>
         <el-input
           style="width:180px"
           v-model="inputValue"
           prefix-icon="el-icon-search"
           placeholder="搜索处方"
         ></el-input>
+        <ul class="tabs flex align-items">
+          <li
+            v-for="(item,index) of tabsData"
+            :key="index"
+            class="item"
+            :class="{'is-selected':tag===index}"
+            @click="tag=index"
+          >
+            <span>{{item}}</span>
+          </li>
+        </ul>
       </div>
+
       <!-- 表格 -->
-      <el-tabs tab-position="left" style="height: 100%;">
-        <el-tab-pane label="全部">
-          <div class="all-wrapper">
-            <div class="table-wrapper">
-              <ul class="th flex align-items">
-                <li class="item1">
-                  <span>处方模板名称</span>
-                </li>
-                <li class="item2 flex-item">
-                  <span>处方内容</span>
-                </li>
-                <li class="item3">
-                  <span>创建人</span>
-                </li>
-              </ul>
-              <div class="table-body">
-                <ul v-for="(item,index) of 6" :key="index" class="td flex align-items">
-                  <li class="item1">
-                    <span>流感</span>
-                  </li>
-                  <li class="flex-item text-ellipsis">
-                    <span>奥美拉唑肠溶胶囊18粒、 4561dsf6sdf5sdf5sdf阿莫西林胶囊12粒、 感冒灵胶囊8片、 维生素B1片12片、 维生素B2片30片 阿莫西林胶囊22粒</span>
-                  </li>
-                  <li class="item3">
-                    <span>李思思</span>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </el-tab-pane>
-        <el-tab-pane label="诊所公用">
-          <div class="sharing-wrapper"></div>
-        </el-tab-pane>
-        <el-tab-pane label="个人专用">
-          <div class="private-wrapper"></div>
-        </el-tab-pane>
-      </el-tabs>
+      <div class="table-wrapper">
+        <ul class="th flex align-items">
+          <li class="item1">
+            <span>处方模板名称</span>
+          </li>
+          <li class="item2 flex-item">
+            <span>处方内容</span>
+          </li>
+          <li class="item3">
+            <span>创建人</span>
+          </li>
+        </ul>
+        <div class="table-body">
+          <ul v-for="(item,index) of 6" :key="index" class="td flex align-items">
+            <li class="item1">
+              <span>流感</span>
+            </li>
+            <li class="flex-item text-ellipsis">
+              <span>奥美拉唑肠溶胶囊18粒、 4561dsf6sdf5sdf5sdf阿莫西林胶囊12粒、 感冒灵胶囊8片、 维生素B1片12片、 维生素B2片30片 阿莫西林胶囊22粒</span>
+            </li>
+            <li class="item3">
+              <span>李思思</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
       <!-- 分页 -->
-      <div class="pag-wrapper flex align-items justify-end">
-        <el-pagination
-          :page-size="100"
-          layout="total, prev, pager, next"
-          :total="1000"
-        ></el-pagination>
+      <div class="pag-wrapper flex align-items">
+        <el-pagination :page-size="100" layout="total, prev, pager, next" :total="1000"></el-pagination>
       </div>
     </div>
   </div>
@@ -68,17 +67,20 @@
 /**************全局组件************/
 /*************局部组件***********/
 import AdminHeader from "../components/adminHeader";
+import DialogAddWest from "./dialog/dialogAddWest";
 export default {
   components: {
-    AdminHeader
+    AdminHeader,
+    DialogAddWest
   },
   data() {
     return {
-      inputValue: "" //搜索处方值
+      inputValue: "", //搜索处方值
+      tag: 0, // 标签切换索引
+      tabsData: ["全部", "诊所公用", "个人专用"], // 标签切换数据
     };
   },
-  methods: {
-  }
+  methods: {}
 };
 </script>
 <style lang="less" scoped>
@@ -87,48 +89,66 @@ export default {
   .west-body {
     padding: 0 24px;
     // 按钮
-    .btn-wrapper {
+    .header-wrapper {
       height: 61px;
-    }
-    /deep/ .el-tabs--left .el-tabs__header {
-      margin-top: 50px;
-      padding-left: 0;
-      border-bottom: 0;
-      background-color: #fff;
-      /deep/ .el-tabs__item {
-        padding-left: 0;
+      .tabs {
+        margin-left: 10px;
+        height: 32px;
+        > li.item {
+          height: 32px;
+          line-height: 32px;
+          text-align: center;
+          width: 70px;
+          color: #8493a4;
+          border: 1px solid #ced0da;
+          cursor: pointer;
+        }
+        li.item + li.item {
+          margin-left: -1px;
+        }
+        li:last-child {
+          border-top-right-radius: 3px;
+          border-bottom-right-radius: 3px;
+        }
+        li:first-child {
+          border-top-left-radius: 3px;
+          border-bottom-left-radius: 3px;
+        }
+        // 激活样式
+        .is-selected {
+          background-color: #f5f7fb;
+          color: #007aff !important;
+        }
       }
     }
-    // 全部表格
-    .all-wrapper {
-      .table-wrapper {
-        min-height: 300px;
-        ul > li {
-          padding: 0 6px;
-        }
+    // 表格
+    .table-wrapper {
+      min-height: 300px;
+      ul > li {
+        padding: 0 6px;
+      }
+      .item1 {
+        width: 18.75%;
+      }
+      .item3 {
+        width: 8.33333%;
+      }
+      .th {
+        height: 40px;
+        color: #8493a4;
+        background: #f5f7fb;
+        border-top: 1px solid #e6eaee;
+        border-bottom: 1px solid #e6eaee;
+      }
+      .td {
         .item1 {
-          width: 18.75%;
+          color: @color_007aff;
         }
-        .item3 {
-          width: 8.33333%;
-        }
-        .th {
-          height: 40px;
-          color: #8493a4;
-          background: #f5f7fb;
-          border-top: 1px solid #e6eaee;
-          border-bottom: 1px solid #e6eaee;
-        }
-        .td {
-          .item1 {
-            color: @color_007aff;
-          }
-          cursor: pointer;
-          height: 48px;
-          border-bottom: 1px solid #dadbe0;
-          &:hover {
-            background: @color_e9eff8;
-          }
+        cursor: pointer;
+        height: 48px;
+        border-bottom: 1px solid #dadbe0;
+        &:hover {
+          background: @color_e9eff8;
         }
       }
     }
